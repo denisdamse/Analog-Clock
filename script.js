1c3b43ed69cd0f71ed50f127f6cb7b0b71811f72
Denis <denisdamse@gmail.com>
2024-10-12 15:44:35 +0300

    minutes line created

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const date = new Date();
 let hours = date.getHours();
+let minutes = date.getMinutes();
 function setup()
 {
     createCanvas(400,500);
 }
 function draw()
 {
+    stroke('black')
    circle(200,250,300);
+   // Drawing hours
    if(hours<12)
    {
     line(200,250,200+Math.sin(hours*30*(Math.PI/180))*150, 250+Math.cos(hours*30*(Math.PI/180))*150);
@@ -14,5 +17,10 @@ function draw()
    else if(hours>12)
    {
     line(200,250,200+Math.sin((hours-12)*30*(Math.PI/180))*150, 250+Math.cos((hours-12)*30*(Math.PI/180))*150);
+   }
+   //Drawing minutes
+   stroke('red');
+   {
+    line(200,250,200+Math.sin(minutes*6*(Math.PI/180))*150, 250+Math.cos(minutes*6*(Math.PI/180))*150);
    }
 }
