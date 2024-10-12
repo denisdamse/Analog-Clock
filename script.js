bf10ecb2a0a7b0671244e7a80f08a9c28164891c
Denis <denisdamse@gmail.com>
2024-10-12 16:08:38 +0300

    all hands work properly

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const date = new Date();
-let hours = date.getHours();
-let minutes = date.getMinutes();
 function setup()
 {
     createCanvas(400,500);
 }
 function draw()
 {
+    const updatedDate= new Date();
+    let updatedHours = updatedDate.getHours();
+    let updatedMinutes = updatedDate.getMinutes();
+    let updatedSeconds = updatedDate.getSeconds();
     stroke('black')
    circle(200,250,300);
    // Drawing hours
-   if(hours<12)
+   if(updatedHours<12)
    {
-    line(200,250,200+Math.sin(hours*30*(Math.PI/180))*150, 250+Math.cos(hours*30*(Math.PI/180))*150);
+    line(200,250,200+Math.sin(updatedHours*30*(Math.PI/180))*150, 250-Math.cos(updatedHours*30*(Math.PI/180))*150);
    }
-   else if(hours>12)
+   else if(updatedHours>12)
    {
-    line(200,250,200+Math.sin((hours-12)*30*(Math.PI/180))*150, 250+Math.cos((hours-12)*30*(Math.PI/180))*150);
+    line(200,250,200+Math.sin((updatedHours-12)*30*(Math.PI/180))*150, 250-Math.cos((updatedHours-12)*30*(Math.PI/180))*150);
    }
    //Drawing minutes
    stroke('red');
    {
-    line(200,250,200+Math.sin(minutes*6*(Math.PI/180))*150, 250+Math.cos(minutes*6*(Math.PI/180))*150);
+    line(200,250,200+Math.sin(updatedMinutes*6*(Math.PI/180))*150, 250-Math.cos(updatedMinutes*6*(Math.PI/180))*150);
+   }
+   //Drawing seconds
+   stroke('blue');
+   {
+    line(200,250,200+Math.sin(updatedSeconds*6*(Math.PI/180))*150, 250-Math.cos(updatedSeconds*6*(Math.PI/180))*150);
    }
 }
