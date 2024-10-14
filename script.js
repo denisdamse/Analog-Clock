b2325e7fd6ab67ab90069a4a3469aa3c35dff677
Denis <denisdamse@gmail.com>
2024-10-14 09:13:20 +0300

    arranged the code, removed unnecessary code

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function draw()
    circle(200,250,350);
    // Drawing hours
    text('12',190,90);
-   text('3',360,260);
+   text('3',360,255);
    text('6',200,420);
-   text('9',35,260);
+   text('9',35,255);
    strokeWeight(5);
    if(updatedHours<12)
    {
@@ -26,12 +26,8 @@ function draw()
    }
    //Drawing minutes
    strokeWeight(3);
-   {
     line(200,250,200+Math.sin(updatedMinutes*6*(Math.PI/180))*150, 250-Math.cos(updatedMinutes*6*(Math.PI/180))*150);
-   }
    //Drawing seconds
    strokeWeight(1);
-   {
     line(200,250,200+Math.sin(updatedSeconds*6*(Math.PI/180))*150, 250-Math.cos(updatedSeconds*6*(Math.PI/180))*150);
-   }
 }
